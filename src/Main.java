e119a4ce35452663f6dfeeccb07c0598323868be
matdibun <adibgempakzz@gmail.com>
2024-12-01 11:02:06 +0900

    updating TODO

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
     }
 }
 
-/*TODO: 1. Leaderboard: (timer not shown);   problem: this.timer > null
-        2. Button press --> timer go, done --> timer stop && reset timer
+/*TODO: 1. Leaderboard: (timer not shown);   problem: this.timer > null     DONE
+        2. Button press --> timer go, done --> timer stop && reset timer    DONE
         3. Put leaderboard inside JFrame; JFrame invoked after leaderboard selected
  */
